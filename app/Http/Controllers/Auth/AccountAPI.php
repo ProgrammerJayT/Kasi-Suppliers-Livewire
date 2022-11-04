8c59bbbf562372047423297b33e40b76275a0a81
Programmer JayT <theanthem8@gmail.com>
2022-11-04 18:32:28 +0200

    Created API controller

---
+++ app/Http/Controllers/Auth/AccountAPI.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\Account;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AccountAPI extends Controller
+{
+    //
+}
