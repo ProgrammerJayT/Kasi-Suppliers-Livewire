c696d7996810d3ab769ccc89c82a27442f000eb9
Programmer JayT <theanthem8@gmail.com>
2022-11-04 17:57:07 +0200

    Added validation rules

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -6,6 +6,22 @@ use Livewire\Component;
 
 class Login extends Component
 {
+    public $email, $password;
+
+    //Add rules for validations
+    protected $rules = [
+        'email' => 'required|email',
+        'password' => 'required|min:8',
+    ];
+
+    //Process user input
+    public function login()
+    {
+        $this->validate();
+
+        //Do something with user input
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
