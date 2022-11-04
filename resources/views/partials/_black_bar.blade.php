e5a466e0bef6b183aee21bd088884266a74dbd28
Programmer JayT <theanthem8@gmail.com>
2022-11-04 17:58:22 +0200

    Created black bar for styling

---
+++ resources/views/partials/_black_bar.blade.php
@@ -0,0 +1,13 @@
+<header class="header">
+    <div class="header__top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-7">
+                    <div class="header__top__left">
+                        <p>Hello there</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
