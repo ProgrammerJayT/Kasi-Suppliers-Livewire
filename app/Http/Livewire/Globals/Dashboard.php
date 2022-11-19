70c582227e0211ec8dae969cdeab0471a0c17d38
Programmer JayT <theanthem8@gmail.com>
2022-11-19 21:01:24 +0200

    Created livewire dashboard component

---
+++ app/Http/Livewire/Globals/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire\Globals;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Request;
+
+class Dashboard extends Component
+{
+    public $route, $privilege, $name, $surname;
+
+    public function render()
+    {
+        if (session()->has('adminTriggered')) {
+            $this->name = 'Login/Register';
+            $this->surname = 'Login/Register';
+            $this->privilege = 'admin';
+        } else {
+            $this->name = session()->get('profile')->name;
+            $this->surname = session()->get('profile')->surname;
+            $this->privilege = session()->get('account')->privilege;
+        }
+
+        $this->route = Request::route()->getName();
+
+        return view('livewire.globals.dashboard');
+    }
+}
