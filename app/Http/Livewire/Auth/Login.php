350a74d24d4a15061778136884a7028cd79df8a0
Programmer JayT <theanthem8@gmail.com>
2022-11-19 21:00:51 +0200

    Created livewire login component

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Http\Controllers\DBHelpers\LoginUser;
+use App\Models\Account;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class Login extends Component
 {
-    public $email, $password;
+    public $email, $password, $admin = '#_admin.k@si.c0m';
 
     //Add rules for validations
     protected $rules = [
@@ -19,7 +22,34 @@ class Login extends Component
     {
         $this->validate();
 
-        //Do something with user input
+        try {
+
+            $loginAttempt = LoginUser::read(
+                [
+                    'email' => $this->email,
+                    'password' => $this->password
+                ]
+            );
+
+            if ($loginAttempt->getStatusCode() == 200) {
+                return redirect()->route('dashboard')->with('welcome', 'Welcome back ' . $loginAttempt->original['profile']->name);
+            } else {
+                $this->password = '';
+                return back()->with('fail', $loginAttempt->original);
+            }
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            $this->password = '';
+            return back()->with('fail', $th->getMessage());
+        }
+    }
+
+    public function adminPortal()
+    {
+        session()->put('adminTriggered', true);
+
+        return redirect()->route('admin');
     }
 
     public function render()
