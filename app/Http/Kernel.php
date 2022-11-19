7459c5908eb99293545dfaf98f8a378f142eccd3
Programmer JayT <theanthem8@gmail.com>
2022-11-19 20:59:03 +0200

    Added admin, auth, and guest middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\CheckAuth;
+use App\Http\Middleware\Guest;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -37,7 +40,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
@@ -63,5 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'checkAuth' => CheckAuth::class,
+        'checkGuest' => Guest::class,
+        'checkAdmin' => Admin::class,
     ];
 }
